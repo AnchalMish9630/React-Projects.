78ce06d563577e327586c97855df36716a03fcfb
Anchal Mishra <anchal.mishra@piramal.com>
2024-11-15 23:52:29 +0530

    EP-2 Added online status icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,7 +13,7 @@ const Header= ()=>{
         </div>
         <div className="flex items-center">
             <ul className="flex p-4 m-4">
-                <li className="px-4"> Online status: {onlineStatus ? "" : ""}</li>
+                <li className="px-4"> Online status: {onlineStatus ? " ✅" : "🔴"}</li>
                 <li className="px-4">
                     <Link to="/">Home</Link>
                 </li>
