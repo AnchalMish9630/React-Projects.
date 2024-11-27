adc6abd888d0075b9e581ddd6e701a9dbeab167d
Anchal Mishra <anchal.mishra@piramal.com>
2024-11-27 23:51:24 +0530

    EP-4 Implemented cart functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,15 @@
 import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import UserContext from "../../utils/UserContext";
+import { useSelector } from "react-redux";
 const Header= ()=>{
 
     const [loginButton, setLoginButton] = useState("Login"); 
     const [onlineStatus, setOnlineStatus] = useState(false)
     const {loggedInUser} = useContext(UserContext);
     // console.log("hello", LoggedInUserName);
+    const cartData = useSelector((store)=> store.cart.itemCart)
+    console.log("hello cart", cartData)
 
     return(
         <div>
@@ -21,7 +24,7 @@ const Header= ()=>{
                     <Link to="/">Home</Link>
                 </li>
                 <li className="px-4 font-bold">
-                    Cart 
+                   <Link>Cart ({cartData.length}) items</Link> 
                 </li>
                 <li className="px-4">
                     <Link to="/about">About Us</Link>
