87fad9024c741ada8603298d754d6da45b5f1f7e
Anchal Mishra <anchal.mishra@piramal.com>
2024-11-30 22:48:41 +0530

    EP-5 Configured jest in app

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,17 +1,29 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import ItemList from "./ItemList";
+import { clearCart } from "../../utils/store/cartSlice";
 
 const Cart = ()=>{
 
     const cartInfo = useSelector((store)=>(store.cart.itemCart));
     console.log("My cart ", cartInfo);
 
+    const dispatch = useDispatch();
+    const handlerClearCart = ()=>{
+        dispatch(clearCart(cartInfo));
+    }
+
+
     return(
         <div className="text-center m-4 p-4">
             <div className="text-2xl font-bold">
                 Cart
                 </div>
-                <div>
+                <div className="w-6/12 m-auto">
+                
+                    <button 
+                        className="ml-[650px] bg-purple-300 text-black p-2 m-2 rounded-lg"
+                        onClick={()=>{handlerClearCart(cartInfo)}}>Clear cart</button>
+                
                 <ItemList itemData={cartInfo} />
                 </div>
               
