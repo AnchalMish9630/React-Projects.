e96e302a61aaea50565cff2766bd1642745b5f5d
Anchal Mishra <anchal.mishra@piramal.com>
2024-12-02 02:09:34 +0530

    EP-5 test cases for contact us page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,13 @@
 const Contact=()=>{
     return(
-        <>Contact</>
+       <div>
+            <h1 className="font-bold text-2xl p-4 m-4">Contact Us page</h1>
+            <form>
+                <input type="text" className=" border-black m-2 p-4" placeholder="name" />
+                <input type="text" className="border-black m-2 p-4" placeholder="message" />
+                <button className="border border-black m-2 p-4 bg-gray-400 rounded-lg">Submit</button>
+            </form>
+       </div>
     )
 }
 export default Contact;
--- src/components/sum.js
+++ src/components/sum.js
@@ -1,3 +1,3 @@
-export const dum = (a,b)=>{
+export const sum = (a,b)=>{
     return a+b;
 };
