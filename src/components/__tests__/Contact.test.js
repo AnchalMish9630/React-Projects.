d077d958c047a9b773ac1e3784a3d4275bc6e34c
Anchal Mishra <anchal.mishra@piramal.com>
2024-12-04 02:18:11 +0530

    EP-5 Added test cases

--- src/components/__tests__/Contact.test.js
+++ src/components/__tests__/Contact.test.js
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import Contact from "../Contact";
 import '@testing-library/jest-dom';
 
@@ -7,4 +7,23 @@ test("It should load contact us component ", ()=>{
     render(<Contact />);
     const heading = screen.getByRole("heading");
     expect(heading).toBeInTheDocument();
+})
+
+test ("To test button is present on the page or not", ()=>{
+    render(<Contact />);
+    const button = screen.getByText("Submit");
+    expect(button).toBeInTheDocument();
+});
+
+test("to find the input box on screen ", ()=>{
+    render(<Contact />);
+    const inputValue = screen.getByPlaceholderText("name");
+    expect(inputValue).toBeInTheDocument();
+})
+
+
+test("to find no. of  input box on screen ", ()=>{
+    render(<Contact />);
+    const inputValue = screen.getAllByRole("textbox");
+    expect(inputValue.length).toBe(2);
 })
